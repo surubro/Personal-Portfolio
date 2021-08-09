a5fa8fd005c1cea1ca0e04298977acd9c10a64d3
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2021-08-09 17:24:37 +0530

    Create Home Screen, Preloader, Navbar
    Signed-off-by: ☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { FaLinkedinIn, FaTelegram } from "react-icons/fa";
+import { BiCopyright } from "react-icons/bi";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>
+            Copyright <BiCopyright /> {year} AP
+          </h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Made with ❤️ by Arka Pramanik</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/AP-XD"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://t.me/AP_XD"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaTelegram />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/arka--pramanik/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/_._._apxd_._._/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
