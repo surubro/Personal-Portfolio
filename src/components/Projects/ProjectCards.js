bb4393924f751555e2598ed552f7a28608ee2c52
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2023-07-05 21:48:27 +0530

    Update Projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,22 +2,26 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { BiLinkExternal } from "react-icons/bi";
-
+import CardGroup from 'react-bootstrap/CardGroup';
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view" style={{ width: '18rem' }}>
+    <CardGroup>
+    <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ padding:'2rem' }} />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Title style={{ fontSize:"1.25rem", fontWeight:"700"}}>{props.title}</Card.Title>
+        <Card.Text style={{ textAlign: "center"}}>
           {props.description}
         </Card.Text>
+        <Card.Footer>
         <Button variant="primary" href={props.link} target="_blank">
           <BiLinkExternal /> &nbsp;
           {"View Project"}
-        </Button>
+        </Button></Card.Footer>
       </Card.Body>
     </Card>
+    </CardGroup>
+
   );
 }
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
+import HPE from "../../Assets/Projects/HPE.png";
+import arrow from "../../Assets/Projects/arrow.jpg";
+import enconnect from "../../Assets/Projects/enconnect.jpg";
+import tuition from "../../Assets/Projects/tuition.jpg";
+import habify from "../../Assets/Projects/habify.jpg";
+import nlp from "../../Assets/Projects/nlp.png";
+import weather from "../../Assets/Projects/weather.png";
 import Netflix from "../../Assets/Projects/Netflix.svg";
 
 function Projects() {
@@ -11,20 +18,91 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Recent <strong className="blue">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Row style={{ justifyContent: "space-around", paddingBottom: "10px" }}>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={HPE}
+              title="HPC Security Dashboard"
+              description="Enhance Kubernetes security monitoring with integrated Kube Bench and Trivy, enabling visualization of metrics in Prometheus and Grafana for actionable insights and improved security posture."
+              link="https://github.com/AP-XD/HPE-CTY-HPC-Security-Dashboard"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={tuition}
+              title="Online Tuition Management System"
+              description="A web app that helps the student and teachers to find, manage and organize local tuition centers."
+              link="https://github.com/AP-XD/SWE-J-Component"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={arrow}
+              title="Device Tree"
+              description="ASUS Max M2 (X01AD) Device Tree Source Code for building Arrow OS Custom ROM. "
+              link="https://github.com/AP-XD/device_asus_X01AD"
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "space-around", paddingBottom: "10px" }}>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={habify}
+              title="Habify"
+              description="Our Habit Tracker App that helps you to be more disciplined and form good habits to be more productive."
+              link="https://github.com/AP-XD/Habify"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={enconnect}
+              title="Enconnect"
+              description="Our project aims in helping make chats the private way by encrypting and decrypting with standard algorithms and using multiple algorithms in same process which provides security and attack free."
+              link="https://github.com/AP-XD/EnConnect"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={nlp}
+              title="Youtube Analyser"
+              description="Our app provides you with a crisp description for your youtube video by using various algorithms."
+              link="https://github.com/AP-XD/CSE4022-Project"
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "space-around", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={weather}
+              title="Weather App"
+              description="Beginner Project: Displays weather of a city using OPEN WEATHER API."
+              link="https://github.com/AP-XD/EnConnect"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Netflix}
               title="Netflix Clone"
-              description="Netflix Clone built using React.js, and Firebase. Fetches latest trending movies from TMDB API."
+              description="Beginner Project: Netflix Clone built using React.js, and Firebase. Fetches latest trending movies from TMDB API."
               link="https://github.com/AP-XD/Netflix-clone"
             />
           </Col>
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={habify}
+              title="Habify"
+              description="Our Habit Tracker App that helps you to be more disciplined and form good habits to be more productive."
+              link="https://github.com/AP-XD/Habify"
+            />
+          </Col> */}
+
         </Row>
       </Container>
     </Container>
