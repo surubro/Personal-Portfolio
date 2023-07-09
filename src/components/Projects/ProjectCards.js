ed0c5b301eb055c613a63297125b7843d96d53b0
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2023-07-10 01:07:32 +0530

    Fixed Height of each cards

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,9 +6,9 @@ import CardGroup from 'react-bootstrap/CardGroup';
 function ProjectCards(props) {
   return (
     <CardGroup>
-    <Card className="project-card-view">
+    <Card className="project-card-view" style={{ height: '30rem' }}>
       <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ padding:'2rem' }} />
-      <Card.Body>
+      <Card.Body style={{display:"flex",flexDirection:"column",justifyContent:"space-around"}}>
         <Card.Title style={{ fontSize:"1.25rem", fontWeight:"700"}}>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "center"}}>
           {props.description}
